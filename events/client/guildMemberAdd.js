3f2abb79644d75bc0fe44e883c721c60f42f831c
Blackin22 <77985817+blackin22@users.noreply.github.com>
2021-03-18 23:21:02 -0300

    Arrumando bugs!

--- events/client/guildMemberAdd.js
+++
@@ -1,8 +0,0 @@
-module.exports = (client, member) => {
-
-const guild = client.guilds.cache.get("id-servidor")
-const canal = guilds.channels.cache.get("id-canal")
-
-canal.send(`${member} Seja bem vindo! ID: ${member.id}`)
- 
-}
