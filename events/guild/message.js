f96c1b022574914af3f546988acdd4db433fbdb3
Blackin22 <77985817+blackin22@users.noreply.github.com>
2021-03-18 23:18:14 -0300

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -3,7 +3,7 @@ let prefix = '!'
 
 module.exports = async (client, message) => {
     if(message.author.bot) return;
-    if(message.channel.type === 'dm') 
+    if(message.channel.type === 'dm') return;
 
     if(!message.content.startsWith(prefix)) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
