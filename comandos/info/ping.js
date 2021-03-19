1e9f6c435ad772ffc47f8b0bfd7ecc17bb423629
Blackin22 <77985817+blackin22@users.noreply.github.com>
2021-03-19 00:31:03 -0300

    Update ping.js

--- comandos/info/ping.js
+++ comandos/info/ping.js
@@ -12,7 +12,9 @@ run: async (client, message, args) => {
    
       let embed2 = new MessageEmbed()
       .setDescription(`> Ping: ${client.ws.ping}`)
+      setTimeout(() => {
       msg.edit(embed2)
+      }, 5000) // 5 segundos.
       } catch (e) {
          message.channel.send("Algo de errado acontenceu :sob:")
       }
